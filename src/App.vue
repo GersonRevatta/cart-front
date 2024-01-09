d82841af9aeeff08dda94e90f060e13cb7fb8d66
gerson <gersonrevatta@gmail.com>
2024-01-09 12:34:40 -0500

    feat: initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+// import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <!-- Hello world -->
+    <RouterView />
+  </div>
+  <!--
+  <header>
+  </header>
+  -->
+    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
+
+    <!-- <div class="wrapper">
+      <HelloWorld msg="You did it! ssd" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div> -->
+
+  <!-- <RouterView /> -->
+</template>
+
+<style scoped>
+</style>
